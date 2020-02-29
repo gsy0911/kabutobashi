f5f2fe200c2a9ec2fa79f6a7d628651ed4043ece
gsy0911 <yoshiki0911@gmail.com>
2020-02-29 21:49:37 +0900

    update pystock

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gsy0911/pystock",
-    package_dir={'': 'pystock'},
-    packages=[''],
+    packages=['pystock'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
