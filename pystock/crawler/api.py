dbd7a44458b4e3ac3b50af106de3181e4361efc3
yoshiki <yoshiki0911@gmail.com>
2020-04-07 23:22:49 +0900

    crawlerの提供APIの更新

--- pystock/crawler/api.py
+++ pystock/crawler/api.py
@@ -1,90 +1,19 @@
 from pystock.crawler.crawler import Crawler
-from typing import Union
 
 # 単一銘柄の詳細情報を取得するクラス
 from pystock.crawler.stock_crawler.stock_detail_crawler import (
-    StockDetailCrawler
+    get_stock_detail
 )
 
 from pystock.crawler.stock_crawler.ipo_list_crawler import (
-    IPOListCrawler
+    get_ipo_list_from_year
 )
 
-# 株価の情報を取得するクラス
-from pystock.crawler.stock_crawler.stock_list_crawler import (
-    StockBrandListCrawler,
-    StockBrandOneIndustryListController,
-    StockBrandAllIndustryController
+from pystock.crawler.stock_crawler.weeks_52_high_low_crawler import (
+    get_52_weeks_high_low
 )
 
-from pystock.crawler.stock_crawler.stock_list_page import (
-    OneRow
-)
 
-# OneRowのリストをDataFrameに変換する関数
-convert_to_df = OneRow.convert_to_df
-
-
-def get_beautifulsoup_result(url: str):
+def get_webpage(url: str) -> str:
     crawler = Crawler()
-    return crawler.get_beautifulsoup_result(url)
-
-
-def get_stock_detail(code: Union[str, int]) -> dict:
-    """
-    単一株の実行日時の詳細情報を取得する関数
-    :params code: 取得したい銘柄コード
-    :return:
-    """
-    url = f"https://minkabu.jp/stock/{code}"
-    stock_detail_crawler = StockDetailCrawler(url=url)
-    return stock_detail_crawler.crawl()
-
-
-def get_ipo_list_from_year(year: Union[str, int]) -> dict:
-    """
-    IPOのリストを取得する関数
-    """
-    base_url = "https://96ut.com/ipo/list.php"
-    year_str = None
-    if type(year) is int:
-        year_str = str(year)
-    else:
-        year_str = year
-
-    if year_str == "2020":
-        # 実行年の場合
-        url = base_url
-    else:
-        # 過去年の場合
-        url = f"{base_url}?year={year}"
-    ipo_list_crawler = IPOListCrawler(url=url)
-    return ipo_list_crawler.crawl()
-
-# def get_stock_brand_list_from_one_page(url: str) -> list:
-#    """
-#    単一のページから銘柄リストを取得する
-#    """
-#    stock_brand_list_crawler = StockBrandListCrawler(url=url)
-#    stock_brand_list_crawler.crawl()
-#    return stock_brand_list_crawler.get_info()
-
-
-# def get_stock_brand_list_from_one_industry_type(url: str) -> list:
-#    """
-#    単一の業種の銘柄リストを取得する関数
-#    """
-#    stock_brand_list_crawler = StockBrandOneIndustryListController(
-#        initial_url=url)
-#    stock_brand_list_crawler.crawl()
-#    return stock_brand_list_crawler.get_info()
-
-
-# def get_stock_brand_list(base_url) -> list:
-#    """
-#    現在上場している銘柄リストを取得する関数
-#    """
-#    stock_brand_list_crawler = StockBrandAllIndustryController(
-#        base_url=base_url)
-#    stock_brand_list_crawler.crawl()
-#    return stock_brand_list_crawler.get_info()
+    return crawler.get_url_text(url)
--- pystock/crawler/crawler.py
+++ pystock/crawler/crawler.py
@@ -31,6 +31,28 @@ class Crawler(AbstractCrawler):
         """
         pass
 
+    def __call__(self, **kwargs) -> dict:
+        url = None
+        text = None
+        if "url" in kwargs:
+            url = kwargs['url']
+            text = self.get_url_text(target_url=url)
+            url = None
+        if "text" in kwargs:
+            text = kwargs['text']
+        # 両方に値が含まれている場合は例外を投げる
+        if (url is not None) and (text) is not None:
+            raise ValueError("両方に値を設定しないでください")
+        result = self.web_scraping(text)
+        return result
+
+    def web_scraping(self, text: str) -> dict:
+        """
+        textより情報を抽出する
+        :params text: webページ
+        """
+        raise NotImplementedError("please implement your code")
+
     def get_url_text(self, target_url: str) -> str:
         """
         requestsを使って、webからページを取得し、htmlを返す
@@ -47,12 +69,12 @@ class Crawler(AbstractCrawler):
         r.encoding = r.apparent_encoding
         return r.text
 
-    def get_beautifulsoup_result(self, target_url: str) -> BeautifulSoup:
-        """
-        beautifulsoupを使える状態にする
-        """
-        text = self.get_url_text(target_url)
-        return BeautifulSoup(text, 'lxml')
+    # def get_beautifulsoup_result(self, target_url: str) -> BeautifulSoup:
+    #     """
+    #     beautifulsoupを使える状態にする
+    #     """
+    #     text = self.get_url_text(target_url)
+    #     return BeautifulSoup(text, 'lxml')
 
     def get_crawl_datetime(self) -> str:
         JST = timezone(timedelta(hours=+9), 'JST')
