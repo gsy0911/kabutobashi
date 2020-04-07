ad405c26ac7a6107d0682ae3ea79490515887d91
yoshiki <yoshiki0911@gmail.com>
2020-04-07 23:22:25 +0900

    ioパッケージの更新

--- pystock/io/s3.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-
-def save_img_to_s3(img_data, key: str):
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket("CHART_SAVE_BUCKET_NAME")
-    # img_data = compute_sma_and_save_charts_to_s3(stock_df)
-    bucket.put_object(Key=key, Body=img_data, ACL="public-read")
-    return {"s3_chart_key": key}
