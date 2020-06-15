1c6cd0e7a0129244d2b2617e808ac4ec9f42e1c1
yoshiki <yoshiki0911@gmail.com>
2020-06-15 22:53:40 +0900

    バージョンとsetup.pyの修正

--- kabutobashi/__init__.py
+++ kabutobashi/__init__.py
@@ -58,7 +58,11 @@ ichimoku = Ichimoku()
 momentum = Momentum()
 psycho_logical = PsychoLogical()
 
-__version__ = "0.1.1"
+
+# comparable tuple
+VERSION = (0, 1, 0)
+# generate __version__ via VERSION tuple
+__version__ = ".".join(map(str, VERSION))
 
 # module level doc-string
 __doc__ = """
