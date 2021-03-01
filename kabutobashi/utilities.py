de177ab8421e255ca03c6e385c864ed7236d0003
yoshiki <yoshiki0911@gmail.com>
2021-03-01 22:56:04 +0900

    #50 統計量を計算するクラスの追加

--- kabutobashi/utilities.py
+++ kabutobashi/utilities.py
@@ -2,8 +2,10 @@ from datetime import datetime, timedelta
 import jpholiday
 import numpy as np
 import pandas as pd
-from .errors import PyStockBaseError, StockDfError
 from scipy.optimize import curve_fit
+
+from .errors import PyStockBaseError, StockDfError
+from .method import *
 
 
 def get_past_n_days(current_date: str, n: int = 60) -> list:
@@ -126,9 +128,8 @@ def train_test_sliding_split(
         yield idx, stock_df[i: offset], stock_df[offset: offset + buy_sell_term_days]
 
 
-def compute_statistical_values(stock_df: pd.DataFrame) -> dict:
+def compute_fitting(array_y: list, prefix: str) -> dict:
     statistical_values = {}
-    array_y = stock_df['close']
     array_x = np.array(range(0, len(array_y)))
 
     def _linear_fit(x, a, b):
@@ -139,11 +140,40 @@ def compute_statistical_values(stock_df: pd.DataFrame) -> dict:
 
     def _cube_fit(x, a, b, c, d):
         return a*x*x*x + b*x*x + c*x + d
-
     linear_param, _ = curve_fit(_linear_fit, array_x, array_y)
-    statistical_values.update({f"linear_{idx}": p for idx, p in enumerate(linear_param)})
+    statistical_values.update({f"{prefix}_linear_{idx}": p for idx, p in enumerate(linear_param[:-1])})
     square_param, _ = curve_fit(_square_fit, array_x, array_y)
-    statistical_values.update({f"square_{idx}": p for idx, p in enumerate(square_param)})
+    statistical_values.update({f"{prefix}_square_{idx}": p for idx, p in enumerate(square_param[:-1])})
     cube_param, _ = curve_fit(_cube_fit, array_x, array_y)
-    statistical_values.update({f"cube_{idx}": p for idx, p in enumerate(cube_param)})
+    statistical_values.update({f"{prefix}_cube_{idx}": p for idx, p in enumerate(cube_param[:-1])})
+    return statistical_values
+
+
+def compute_statistical_values(stock_df: pd.DataFrame, fitting_term: int = 10, taken: int = 5) -> dict:
+    # create and initialize instance
+    _sma = SMA(short_term=5, medium_term=21, long_term=70)
+    _macd = MACD(short_term=12, long_term=26, macd_span=9)
+    _adx = ADX()
+    _stochastics = Stochastics()
+
+    statistical_values = {}
+    statistical_values.update(compute_fitting(stock_df['close'], prefix="close"))
+    # params with SMA0
+    sma_df = stock_df.pipe(_sma)
+    statistical_values.update(compute_fitting(sma_df['sma_short'][-fitting_term:], "sma_short"))
+    statistical_values.update(compute_fitting(sma_df['sma_medium'][-fitting_term:], "sma_medium"))
+    statistical_values.update(compute_fitting(sma_df['sma_long'][-fitting_term:], "sma_long"))
+    # params with MACD
+    macd_df = stock_df.pipe(_macd)
+    statistical_values.update({f"histogram_{idx}": v for idx, v in enumerate(macd_df[-taken:]['histogram'].values)})
+    adx_df = stock_df.pipe(_adx)
+    statistical_values.update({f"plus_di_{idx}": v for idx, v in enumerate(adx_df[-taken:]['plus_di'].values)})
+    statistical_values.update({f"minus_di_{idx}": v for idx, v in enumerate(adx_df[-taken:]['minus_di'].values)})
+    statistical_values.update({f"ADX_{idx}": v for idx, v in enumerate(adx_df[-taken:]['ADX'].values)})
+    statistical_values.update({f"ADXR_{idx}": v for idx, v in enumerate(adx_df[-taken:]['ADXR'].values)})
+    # params with Stochastics
+    stochastics_df = stock_df.pipe(_stochastics)
+    statistical_values.update({f"D_{idx}": v for idx, v in enumerate(stochastics_df[-taken:]['D'].values)})
+    statistical_values.update({f"SD_{idx}": v for idx, v in enumerate(stochastics_df[-taken:]['SD'].values)})
+
     return statistical_values
