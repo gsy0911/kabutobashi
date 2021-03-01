cfc1d23f91a3d544b8e3904bde7859d5c73dec93
yoshiki <yoshiki0911@gmail.com>
2021-03-01 23:01:54 +0900

    update version

--- kabutobashi/__init__.py
+++ kabutobashi/__init__.py
@@ -68,7 +68,7 @@ psycho_logical = PsychoLogical()
 fitting = Fitting()
 
 # comparable tuple
-VERSION = (0, 1, 5)
+VERSION = (0, 1, 6)
 # generate __version__ via VERSION tuple
 __version__ = ".".join(map(str, VERSION))
 
