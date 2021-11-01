fea1ba40f3bad110d1aa0a5f84b8119c33b60b58
yoshiki <yoshiki0911@gmail.com>
2021-11-01 23:20:12 +0900

    #49 add visualize function to StockProcessed

--- kabutobashi/domain/entity/stock_processed.py
+++ kabutobashi/domain/entity/stock_processed.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass, field
 from typing import Dict, List, Optional, Union
 
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from cerberus import Validator
+from mplfinance.original_flavor import candlestick_ohlc
+
+from kabutobashi.errors import KabutobashiEntityError
 
 
 @dataclass(frozen=True)
@@ -21,8 +28,14 @@ class StockProcessed:
     PROCESSED_SCHEMA = {
         "method": {"type": "string"},
         "data": {"required": True},
-        "color_mapping": {"type": "list", "schema": {"type": "dict"}},
-        "visualize_option": {"type": "dict", "schema": {"position": {"type": "string", "allowed": ["in", "lower"]}}}
+        "color_mapping": {
+            "type": "list",
+            "schema": {
+                "type": "dict",
+                "schema": {"df_key": {"type": "string"}, "color": {"type": "string"}, "label": {"type": "string"}},
+            },
+        },
+        "visualize_option": {"type": "dict", "schema": {"position": {"type": "string", "allowed": ["in", "lower"]}}},
     }
 
     @staticmethod
@@ -89,9 +102,7 @@ class StockProcessed:
         """
         result_dict = {}
         for v in self.processed_dfs:
-            result_dict.update({
-                v['method']: self._get_impact(_df=v['data'], influence=influence, tail=tail)
-            })
+            result_dict.update({v["method"]: self._get_impact(_df=v["data"], influence=influence, tail=tail)})
         return result_dict
 
     @staticmethod
@@ -112,3 +123,71 @@ class StockProcessed:
         buy_impact_index = _df["buy_impact"].iloc[-tail:].sum()
         sell_impact_index = _df["sell_impact"].iloc[-tail:].sum()
         return round(buy_impact_index - sell_impact_index, 5)
+
+    @staticmethod
+    def add_ax_candlestick(ax, _df: pd.DataFrame):
+        # datetime -> float
+        time_series = mdates.date2num(_df["dt"])
+        data = _df[["open", "high", "low", "close"]].values.T
+        # data
+        ohlc = np.vstack((time_series, data)).T
+        candlestick_ohlc(ax, ohlc, width=0.7, colorup="g", colordown="r")
+
+    def visualize(self):
+        """
+        macdはlower
+        sma、bolinger_bandsは同じところに表示させる。
+        買いのポイントも表示させる
+
+        Returns:
+
+        """
+
+        def _n_rows() -> int:
+            lower_nums = len([p for p in self.processed_dfs if p["visualize_option"]["position"] == "lower"])
+            return 1 + lower_nums
+
+        n_rows = _n_rows()
+
+        def _gridspec_kw() -> dict:
+            if n_rows == 1:
+                return {"height_ratios": [3]}
+            return {"height_ratios": [3] + [1] * (n_rows - 1)}
+
+        gridspec_kw = _gridspec_kw()
+        print(f"n_rows: {n_rows}, gridspec_kw: {gridspec_kw}")
+        fig, axs = plt.subplots(nrows=n_rows, ncols=1, figsize=(6, 5), gridspec_kw=gridspec_kw)
+        # auto-formatting x-axis
+        fig.autofmt_xdate()
+        # set candlestick base
+        self.add_ax_candlestick(axs[0], self.base_df)
+        # display labels
+        axs[0].legend(loc="best")
+
+        ax_idx = 1
+        # plots
+        for processed in self.processed_dfs:
+            position = processed["visualize_option"]["position"]
+            df = processed["data"]
+            time_series = mdates.date2num(df["dt"])
+            mapping = processed["color_mapping"]
+            if position == "in":
+                for m in mapping:
+                    df_key = m["df_key"]
+                    color = m["color"]
+                    label = m["label"]
+                    print(m)
+                    axs[0].plot(time_series, df[df_key], label=label)
+            elif position == "lower":
+                for m in mapping:
+                    df_key = m["df_key"]
+                    color = m["color"]
+                    label = m["label"]
+                    print(m)
+                    axs[ax_idx].plot(time_series, df[df_key], label=label)
+                # lower
+                ax_idx += 1
+            else:
+                raise ValueError()
+
+        return fig
