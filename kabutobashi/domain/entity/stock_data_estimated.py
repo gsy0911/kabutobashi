b87943e1b0b7602bbbd35ec109e995e05a794f60
gsy0911 <yoshiki0911@gmail.com>
2022-02-13 12:09:48 +0900

    #84 add weighted_estimated_value

--- kabutobashi/domain/entity/stock_data_estimated.py
+++ kabutobashi/domain/entity/stock_data_estimated.py
@@ -10,9 +10,18 @@ class StockDataEstimatedBySingleFilter:
     estimated_value: float
     estimate_filter_name: str
 
+    def weighted_estimated_value(self, weights: dict) -> float:
+        weight = weights.get(self.estimate_filter_name, 1)
+        return weight * self.estimated_value
+
 
 @dataclass(frozen=True)
 class StockDataEstimatedByMultipleFilter:
     """ """
 
     estimated: List[StockDataEstimatedBySingleFilter]
+
+    def weighted_estimated_value(self, weights: dict = None) -> float:
+        if not weights:
+            weights = {}
+        return sum([e.weighted_estimated_value(weights=weights) for e in self.estimated])
