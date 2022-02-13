e52711a523cf2eacb713b454a778f5f2abaa68bd
gsy0911 <yoshiki0911@gmail.com>
2022-02-13 23:12:02 +0900

    #89 format

--- kabutobashi/repository/__init__.py
+++ kabutobashi/repository/__init__.py
@@ -1,3 +1,7 @@
-from .stock_data_repository import StockDataMultipleCodeReader, StockDataMultipleCodeWriter, StockDataMultipleCodeCrawler
+from .stock_data_repository import (
+    StockDataMultipleCodeCrawler,
+    StockDataMultipleCodeReader,
+    StockDataMultipleCodeWriter,
+)
 
 reader = StockDataMultipleCodeReader()
