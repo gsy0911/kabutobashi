4c5291d6191e6ea0fe2953f2e8897240f8a9807a
gsy0911 <yoshiki0911@gmail.com>
2022-05-23 21:43:45 +0900

    #106 move and add @dataclass

--- kabutobashi/domain/values/__init__.py
+++ kabutobashi/domain/values/__init__.py
@@ -2,3 +2,4 @@ from .stock_data_estimated import StockDataEstimatedBySingleFilter
 from .stock_data_processed import StockDataProcessedBySingleMethod
 from .stock_data_single_code import StockDataSingleCode
 from .stock_data_visualized import StockDataVisualized
+from .stock_recordset import IStockRecordsetRepository, StockRecordset
