7ae171861f207314f87cc37231c2746e00f71a12
gsy0911 <yoshiki0911@gmail.com>
2022-08-03 01:21:13 +0900

    #118 modify storage repository

--- kabutobashi/infrastructure/repository/stock_recordset_repository.py
+++ kabutobashi/infrastructure/repository/stock_recordset_repository.py
@@ -1,5 +1,4 @@
 import functools
-from abc import abstractmethod
 from concurrent.futures import ThreadPoolExecutor
 from logging import getLogger
 from typing import Generator, List, Union
@@ -13,17 +12,23 @@ from kabutobashi.domain.values import IStockRecordsetRepository, StockInfoHtmlPa
 logger = getLogger(__name__)
 
 
-__all__ = ["IStockRecordsetStorageRepository", "StockRecordsetStorageBasicRepository", "StockRecordsetCrawler"]
+__all__ = ["StockRecordsetStorageBasicRepository", "StockRecordsetCrawler"]
 
 
-class IStockRecordsetStorageRepository(IStockRecordsetRepository):
-    def __init__(self, use_mp: bool, max_workers: int):
+class StockRecordsetStorageBasicRepository(IStockRecordsetRepository):
+    def __init__(self, path_candidate: Union[str, list], use_mp: bool = False, max_workers: int = 2):
+        self.path_candidate = path_candidate
         self.use_mp = use_mp
         self.max_workers = max_workers
 
-    @abstractmethod
     def _read_path_generator(self) -> Generator[str, None, None]:
-        raise NotImplementedError()
+        if type(self.path_candidate) is str:
+            yield self.path_candidate
+        elif type(self.path_candidate) is list:
+            for path in self.path_candidate:
+                yield path
+        else:
+            raise ValueError()
 
     def _stock_recordset_read(self) -> StockRecordset:
         df_list = []
@@ -38,38 +43,19 @@ class IStockRecordsetStorageRepository(IStockRecordsetRepository):
         df = pd.concat(df_list)
         return StockRecordset.of(df=df)
 
-    @abstractmethod
     def _write_path_generator(self) -> Generator[str, None, None]:
-        raise NotImplementedError()
+        if type(self.path_candidate) is str:
+            yield self.path_candidate
+        elif type(self.path_candidate) is list:
+            for path in self.path_candidate:
+                yield path
+        else:
+            raise ValueError()
 
     def _stock_recordset_write(self, data: StockRecordset):
         df = data.to_df(minimum=False)
         for p in self._write_path_generator():
             df.to_csv(p, index=False)
-
-
-class StockRecordsetStorageBasicRepository(IStockRecordsetStorageRepository):
-    def __init__(self, path_candidate: Union[str, list], use_mp: bool = False, max_workers: int = 2):
-        super().__init__(use_mp=use_mp, max_workers=max_workers)
-        self.path_candidate = path_candidate
-
-    def _read_path_generator(self) -> Generator[str, None, None]:
-        if type(self.path_candidate) is str:
-            yield self.path_candidate
-        elif type(self.path_candidate) is list:
-            for path in self.path_candidate:
-                yield path
-        else:
-            raise ValueError()
-
-    def _write_path_generator(self) -> Generator[str, None, None]:
-        if type(self.path_candidate) is str:
-            yield self.path_candidate
-        elif type(self.path_candidate) is list:
-            for path in self.path_candidate:
-                yield path
-        else:
-            raise ValueError()
 
 
 class StockRecordsetCrawler:
