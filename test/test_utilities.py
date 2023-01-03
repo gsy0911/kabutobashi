376ef7ed1cb2a568b45f52f3bd576b0cf9080000
gsy0911 <yoshiki0911@gmail.com>
2023-01-03 17:14:35 +0900

    #148 update test

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -1,4 +1,8 @@
+import pytest
+
 import kabutobashi as kb
+from kabutobashi.domain.errors import KabutobashiEntityError
+from kabutobashi.utilities import convert_float, convert_int, replace
 
 
 def test_workday():
@@ -15,3 +19,43 @@ def test_workday():
     assert "2020-03-22" not in date_list
     assert "2020-03-28" not in date_list
     assert "2020-03-29" not in date_list
+
+
+def test_replace():
+    assert replace("-") == "0"
+    assert replace("") == "0"
+    assert replace("---") == "0"
+    assert replace("1,000") == "1000"
+    assert replace("1000円") == "1000"
+    assert replace("1000株") == "1000"
+    assert replace("1000倍") == "1000"
+    assert replace("1,000倍株円") == "1000"
+
+
+def test_convert_int():
+    # pass
+    assert convert_int("-") == 0
+    assert convert_int("") == 0
+    assert convert_int("---") == 0
+    assert convert_int("1,000") == 1000
+    assert convert_int("1000円") == 1000
+    assert convert_int("1000株") == 1000
+    assert convert_int("1000倍") == 1000
+    assert convert_int("1,000倍株円") == 1000
+    assert type(convert_int(float(0))) is int
+    assert convert_int(float(0)) == 0
+
+    # error
+    with pytest.raises(KabutobashiEntityError):
+        convert_int(input_value={})
+
+    with pytest.raises(KabutobashiEntityError):
+        convert_int(input_value="a")
+
+
+def test_convert_float():
+    with pytest.raises(KabutobashiEntityError):
+        convert_float(input_value={})
+
+    with pytest.raises(KabutobashiEntityError):
+        convert_float(input_value="a")
