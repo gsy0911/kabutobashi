c3eabacaf84152923deb70b557ed67f13ae0eecd
gsy0911 <yoshiki0911@gmail.com>
2023-01-09 10:57:51 +0900

    #156 add Decode-ValueObject

--- kabutobashi/domain/values/__init__.py
+++ kabutobashi/domain/values/__init__.py
@@ -1,4 +1,9 @@
-from .decoded_html_pages import DecodedHtmlPage, DecodeHtmlPageStockInfoMinkabuTop, DecodeHtmlPageStockIpo
+from .decoded_html_pages import (
+    DecodedHtmlPage,
+    DecodeHtmlPageStockInfoMinkabuTop,
+    DecodeHtmlPageStockInfoMultipleDays,
+    DecodeHtmlPageStockIpo,
+)
 from .raw_html_pages import (
     IHtmlPageRepository,
     RawHtmlPage,
