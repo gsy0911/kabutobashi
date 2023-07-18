b264870c8a64bf336c44224dd31dc3c53a2ecd87
gsy0911 <yoshiki0911@gmail.com>
2023-07-18 13:43:43 +0900

    update version

--- kabutobashi/__init__.py
+++ kabutobashi/__init__.py
@@ -45,7 +45,7 @@ sa_volume = SaVolume()
 stock_analysis = [sa_fundamental, sa_volume]
 
 # comparable tuple
-VERSION = (0, 7, 0)
+VERSION = (0, 7, 1)
 # generate __version__ via VERSION tuple
 __version__ = ".".join(map(str, VERSION))
 
