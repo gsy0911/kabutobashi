a14988f4979c0aa02f334f5ff51d60a37c37d8f5
gsy0911 <yoshiki0911@gmail.com>
2024-04-28 09:43:32 +0900

    #184 modify Flow

--- kabutobashi/domain/services/flow/flow.py
+++ kabutobashi/domain/services/flow/flow.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, replace
+from typing import List, Optional, Union
 
 from kabutobashi.domain.services.blocks.abc_block import BlockGlue, IBlock
 
@@ -12,9 +13,17 @@ class Flow:
         glue = BlockGlue(series=None, params=params)
         return Flow(block_glue=glue)
 
-    def then(self, block: IBlock) -> "Flow":
-        new_glue = block.glue(glue=self.block_glue)
-        return replace(self, block_glue=new_glue)
+    def then(self, block: Union[IBlock, List[IBlock]]) -> "Flow":
+        if type(block) is list:
+            flow = self
+            glue = self.block_glue
+            for v in block:
+                glue = v.glue(glue=glue)
+                flow = replace(flow, block_glue=glue)
+            return flow
+        else:
+            new_glue = block.glue(glue=self.block_glue)
+            return replace(self, block_glue=new_glue)
 
     def reduce(self, block: IBlock) -> BlockGlue:
         new_glue = block.glue(glue=self.block_glue)
