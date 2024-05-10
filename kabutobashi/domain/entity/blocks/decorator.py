f2ed68ce19a548d34ab55b17fcdf802c7f1b1154
gsy0911 <yoshiki0911@gmail.com>
2024-05-10 12:29:26 +0900

    #194 modify decorator

--- kabutobashi/domain/entity/blocks/decorator.py
+++ kabutobashi/domain/entity/blocks/decorator.py
@@ -1,4 +1,5 @@
 from types import FunctionType
+from typing import Tuple, Union
 
 import pandas as pd
 
@@ -28,7 +29,7 @@ def _set_new_attribute(cls, name, value):
 
 
 def _inner_func_process(self) -> BlockGlue:
-    res = self._process()
+    res: Union[dict, pd.DataFrame, Tuple[dict, pd.DataFrame], Tuple[pd.DataFrame, dict]] = self._process()
     if type(res) is tuple:
         if len(res) == 2:
             if type(res[0]) is dict and type(res[1]) is pd.DataFrame:
@@ -57,29 +58,36 @@ def _inner_class_func_operate(cls, glue: BlockGlue) -> BlockGlue:
     return new_glue
 
 
-def _process_class(cls, block_name: str = None):
+def _inner_repr(self):
+    return f"""
+    block_name: {self.block_name}
+    """.strip()
+
+
+def _process_class(cls, block_name: str, factory: bool, process: bool):
     cls_params = {}
     cls_annotations = cls.__dict__.get("__annotations__", {})
-    for name, type_ in cls_annotations.items():
-        print(f"{name=}, {type_=}")
-    print("==============")
 
     cls_keys = cls.__dict__.keys()
+    # check _process
     if "_process" not in cls_keys:
-        raise KeyError()
-    if "_factory" not in cls_keys:
         raise KeyError()
     if not isinstance((cls.__dict__["_process"]), FunctionType):
         raise ValueError()
+    _process_annotation_candidates = [Tuple[dict, pd.DataFrame], Tuple[pd.DataFrame, dict], dict, pd.DataFrame]
+    if "return" in cls.__dict__["_process"].__annotations__:
+        if not any([cls.__dict__["_process"].__annotations__["return"] is t for t in _process_annotation_candidates]):
+            print(f"{cls.__dict__['_process'].__annotations__['return']} is not compatible")
+
+    # check _factory
+    if "_factory" not in cls_keys:
+        raise KeyError()
     if not type(cls.__dict__["_factory"]) is classmethod:
         raise ValueError()
 
     for name, value in cls.__dict__.items():
         if name in cls_annotations:
-            #     raise TypeError(f'{name} is a field but has no type annotation')
             cls_params[name] = value
-
-            print(f"{name=}, {value=}")
 
     # set-block-name
     setattr(cls, "block_name", block_name)
@@ -94,14 +102,15 @@ def _process_class(cls, block_name: str = None):
     # validation functions
     _set_new_attribute(cls=cls, name="validate_block_input", value=_inner_func_process)
     _set_new_attribute(cls=cls, name="validate_block_output", value=_inner_func_process)
-
+    # dunder-method
+    _set_new_attribute(cls=cls, name="__repr__", value=_inner_repr)
     blocks_dict.update({block_name: cls})
     return cls
 
 
-def block(cls=None, /, *, block_name: str = None):
+def block(cls=None, /, *, block_name: str = None, factory: bool = False, process: bool = True):
     def wrap(_cls):
-        return _process_class(_cls, block_name=block_name)
+        return _process_class(_cls, block_name=block_name, factory=factory, process=process)
 
     # See if we're being called as @dataclass or @dataclass().
     if cls is None:
