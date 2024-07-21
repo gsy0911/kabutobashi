70ba8d4cfe7f0ee41411d68f81e2e282cb45c8c2
gsy0911 <yoshiki0911@gmail.com>
2024-07-22 08:45:52 +0900

    add read_sqlite3 method

--- kabutobashi/domain/services/flow/flow.py
+++ kabutobashi/domain/services/flow/flow.py
@@ -203,6 +203,19 @@ class FlowPath:
         )
         return replace(self, next_sequence_no=next_sequence_no + 1, flow_params_list=flow_params_list)
 
+    def read_sqlite3(self, code: int, database_dir) -> "FlowPath":
+        next_sequence_no = self.next_sequence_no
+        flow_params_list = self.flow_params_list
+        flow_params_list.append(
+            {
+                "id": "read_sqlite3",
+                "block_name": "read_example",
+                "sequence_no": next_sequence_no,
+                "params": {"code": code, "database_dir": database_dir},
+            }
+        )
+        return replace(self, next_sequence_no=next_sequence_no + 1, flow_params_list=flow_params_list)
+
     def apply_default_pre_process(self) -> "FlowPath":
         next_sequence_no = self.next_sequence_no
         flow_params_list = self.flow_params_list
@@ -210,9 +223,6 @@ class FlowPath:
             {"id": "default_pre_process", "block_name": "default_pre_process", "sequence_no": next_sequence_no}
         )
         return replace(self, next_sequence_no=next_sequence_no + 1, flow_params_list=flow_params_list)
-
-    def read(self) -> "FlowPath":
-        pass
 
     def dumps(self) -> List[dict]:
         return self.flow_params_list
